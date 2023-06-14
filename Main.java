6f5018b822b07f06b931c3f9a0e0131f31924cda
pachogd <legomezd@hotmail.es>
2023-06-14 14:17:55 -0500

    Calculadora de Edad de Perros

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite la Edad de su Perro: ");
+
+        Integer edad = entrada.nextInt();
+
+        edad = edad * 7;
+
+        System.out.println("La Edad de su Perro en Años es :" + edad);
+    }
+}
